35c217b3a070649bc0594d2430990ea7fbcc6d1c
Matthew Taylor <majikpig@gmail.com>
2020-06-07 19:50:50 -0400

    port update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require("express")
 const cors = require("cors")
 const postsRouter = require("./posts/posts-router")
 const generalRouter = require("./general/general-router")
 
 
+const port = process.env.PORT || 4001;
+
 const server = express()
-const port = 4001
 
 server.use(express.json())
 server.use(cors())
